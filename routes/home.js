1e3d7d32c11d90bd348f7e117997184224b426dc
Avishek <avishekprasadbarnwal@gmail.com>
2020-08-11 18:58:10 +0530

    router verification check

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('This is the home route')
+    res.send('This is the home route and verification done')
 })
 
 
