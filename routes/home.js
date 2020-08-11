9ace25fa61093b2f13c1b07cbe884f35676a9bee
Avishek <avishekprasadbarnwal@gmail.com>
2020-08-11 18:44:02 +0530

    first commit

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-
 router.get('/', (req, res) => {
-    res.render('../views/home')
+    res.send('This is the home route')
 })
+
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,10 +5,20 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const bodyParser = require('body-parser')
 const expressLayouts = require('express-layouts')
-const homeRouter = require('./routes/homeRouter')
 const mongoose = require('mongoose')
 
+const homeRouter = require('./routes/home')
+const userRouter = require('./routes/users')
+
 const app = express()
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -16,21 +26,13 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-const db = mongoose.connection
-db.on('error', error => console.log(error))
-db.once('open', () => console.log('Connected to mongoose'))
+app.use('/', homeRouter)
+app.use('/user', userRouter)
 
 
 
-app.get('/', homeRouter)
-
-
-let port = process.env.PORT || 3000
+port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log('server running at port ', port)
+    console.log('server running at port ' + port)
 })
 
