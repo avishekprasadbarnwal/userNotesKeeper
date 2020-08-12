062ec74d8997030deda2cab464712702a6aa78c4
Avishek <avishekprasadbarnwal@gmail.com>
2020-08-12 16:41:28 +0530

    normal user authentication

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,80 @@
 const express = require('express')
+const randomstring = require('randomstring')
 const router = express.Router()
+const User = require('../models/users')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
-router.get('/', (req, res) => {
-    res.render('registers')
+
+
+router.get('/register', (req, res) => {
+    res.render('register', { user: new User()})
 })
 
+router.post('/register', async(req, res) => {
+    const secretToken = randomstring.generate()
+    bcrypt.hash(req.body.password, saltRounds, async(err, hash) => {
+        // Store hash in your password DB.
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            secretToken: secretToken,
+            active: false        
+        })
+        try{
+    
+            // const emailPresentCheck = await User.findOne({'email': email})
+            // if(emailPresentCheck){
+            //     res.send('this email already exists')
+            //     res.redirect('/')
+            //     return;
+            // }
+    
+            //const hash = await user.hashPassword(password);
+    
+            const newUser = await user.save()
+            
+    
+            res.redirect('login')
+        } catch {
+            res.render('register', {
+                    user: user,
+                    errorMessage: "error creating the user" 
+            })
+        }
+    });
+    
+    
+})
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email: email}, (err, foundEmail) => {
+        if(err){
+            res.send('email could not be found')
+        } else {
+            if(foundEmail){
+                bcrypt.compare(password, foundEmail.password, function(err, result) {
+                    // result == true
+                    if(result === true) {
+                        res.render('home')
+                    } else {
+                        res.send('Kindly enter the correct password')
+                    }
+                });
+            }
+        }
+    })
+
+
+})
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,25 +6,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressLayouts = require('express-layouts')
 const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 const homeRouter = require('./routes/home')
 const userRouter = require('./routes/users')
 
 const app = express()
 
-
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
-const db = mongoose.connection
-db.on('error', error => console.log(error))
-db.once('open', () => console.log('Connected to mongoose'))
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 app.use('/', homeRouter)
 app.use('/user', userRouter)
